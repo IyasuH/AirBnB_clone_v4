d45da41c1e39ae9f174d019e82d8e40a089c04cf
IyasuH <eyasuhailegbr@gmail.com>
2022-04-21 21:57:07 -0700

    semistandard FIX

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
-	const amenityList = [];
-	$("div .amenities .popover ul li input").change(function() {
-		const amenityId = $(this).attr('data-id');
-		if ($(this).is(':checked')) {
-			amenityList.push(amenityId);
-		} else {
-			amenityList.splice(output_items.indexOf(amenityId), 1);
-		}
-		$('div .amenities h4').text(amenityList);
-	});
+$(document).ready(function () {
+  const amenityList = [];
+  $('div .amenities .popover ul li input').change(function () {
+    const amenityId = $(this).attr('data-id');
+    if ($(this).is(':checked')) {
+      amenityList.push(amenityId);
+    } else {
+      const index = amenityList.indexOf(amenityId);
+      amenityList.splice(index, 1);
+    }
+    $('div .amenities h4').text(amenityList);
+  });
 });
