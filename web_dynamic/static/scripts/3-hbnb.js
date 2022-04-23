cee18549991e1da8447c4a17cdd3549d6b438ec2
IyasuH <eyasuhailegbr@gmail.com>
2022-04-22 22:18:04 -0700

    Places Ftech

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  const amenityList = [];
+  $('div .amenities .popover ul li input').change(function () {
+    const amenityId = $(this).attr('data-id');
+    if ($(this).is(':checked')) {
+      amenityList.push(amenityId);
+    } else {
+      const index = amenityList.indexOf(amenityId);
+      amenityList.splice(index, 1);
+    }
+    $('div .amenities h4').text(amenityList);
+  });
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+  $.post('http://0.0.0.0:5001/api/v1/places_search/',
+    { 'Content-Type': 'application/json' },
+    function (data, status, jqXHR) {
+      $('section.places').append('<article>' + data + '</article>');
+    });
+});
